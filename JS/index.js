8b936e0899db07ae6760aa746c0c291194f35499
Polinkhan <polinsir@gmail.com>
2022-12-31 01:31:32 +0600

    portfolio

--- JS/index.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function () {
-  $(".itemBox").click(function () {
-    $(".itemBox").removeClass("btnActive fa-beat");
-    $(this).addClass("btnActive fa-beat");
-  });
-
-  $(".projectBox");
-});
